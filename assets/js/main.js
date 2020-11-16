1cf23b9217000764b48896b1e7f9f928bdcc8078
atbahmed <atbahmed@hotech.systems>
2020-11-16 23:42:35 +0300

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,21 +143,21 @@ TxtType.prototype.tick = function() {
     }, delta);
 };
 
-// window.onload = function() {
-//     var elements = document.getElementsByClassName('typewrite');
-//     for (var i=0; i<elements.length; i++) {
-//         var toRotate = elements[i].getAttribute('data-type');
-//         var period = elements[i].getAttribute('data-period');
-//         if (toRotate) {
-//           new TxtType(elements[i], JSON.parse(toRotate), period);
-//         }
-//     }
-//     // INJECT CSS
-//     var css = document.createElement("style");
-//     css.type = "text/css";
-//     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #00cc5c}";
-//     document.body.appendChild(css);
-// };
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #00cc5c}";
+    document.body.appendChild(css);
+};
 
 window.addEventListener('scroll', scrollWindow)
 
